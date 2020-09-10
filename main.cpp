807fce7398c9b64575221e64a2e36c08fd6288b0
bodguy <alsrudgns99@gmail.com>
2020-09-10 10:35:37 +0900

    complete

--- main.cpp
+++ main.cpp
@@ -4,6 +4,249 @@
 #include <GLFW/glfw3.h>
 #include <glm/gtc/type_ptr.hpp>
 
+class Particle {
+public:
+  Particle() = default;
+  explicit Particle(const glm::vec3& position);
+
+  glm::vec3 get_position() const { return m_position; }
+
+  void offset_pos(const glm::vec3& v);
+  void set_movable(bool movable);
+
+  void add_force(const glm::vec3& force);
+  void update(float dt);
+
+private:
+  glm::vec3 m_position{}, m_old_position{}, m_acceleration = glm::vec3(0, 0, 0);
+  float m_mass = 1.f, m_damping = 0.01f;
+  bool m_is_movable = true;
+};
+
+Particle::Particle(const glm::vec3 &position) : m_position{position}, m_old_position{position} {
+
+}
+
+void Particle::offset_pos(const glm::vec3 &v) {
+    if (m_is_movable) {
+        m_position += v;
+    }
+}
+
+void Particle::set_movable(bool movable) {
+    m_is_movable = movable;
+}
+
+void Particle::add_force(const glm::vec3& force) {
+    m_acceleration += force / m_mass;
+}
+
+void Particle::update(float dt) {
+    if (m_is_movable) {
+        glm::vec3 old_position = m_position;
+        m_position = m_position + (m_position - m_old_position) * (1.0f - m_damping) + m_acceleration * dt;
+        m_old_position = old_position;
+        m_acceleration = glm::vec3(0, 0, 0);
+    }
+}
+
+class Constraint {
+public:
+  Constraint(Particle* p1, Particle* p2);
+  void satisfy();
+
+private:
+  float m_rest_distance;
+  Particle* m_p1, *m_p2;
+};
+
+Constraint::Constraint(Particle *p1, Particle *p2) : m_p1{p1}, m_p2{p2} {
+    glm::vec3 v = m_p1->get_position() - m_p2->get_position();
+    m_rest_distance = glm::length(v);
+}
+
+void Constraint::satisfy() {
+    glm::vec3 p1_to_p2 = m_p2->get_position() - m_p1->get_position();
+    float current_distance = glm::length(p1_to_p2);
+    glm::vec3 correction_vec_half = p1_to_p2 * (1.f - m_rest_distance / current_distance) * 0.5f;
+    m_p1->offset_pos(correction_vec_half);
+    m_p2->offset_pos(-correction_vec_half);
+}
+
+class Cloth {
+public:
+  Cloth(int w, int h);
+  ~Cloth();
+
+  void initVertex();
+  void add_wind_force(const glm::vec3& direction);
+
+  void render();
+  void update(float dt);
+
+  Particle* get_particle(int x, int y);
+
+private:
+  glm::vec3 calc_triangle_normal(Particle* p1, Particle* p2, Particle* p3) const;
+  void add_wind_force_for_triangle(Particle* p1, Particle* p2, Particle* p3, const glm::vec3& direction);
+
+private:
+  int m_width, m_height;
+  bool m_enabled = true, m_use_gravity = true;
+  int m_cloth_solver_freq = 15;
+  std::vector<Particle> m_particles;
+  std::vector<Constraint> m_constraint;
+  unsigned int vao = 0, vbo = 0;
+};
+
+Cloth::Cloth(int w, int h) : m_width{w}, m_height{h} {
+    m_particles.resize(m_width * m_height);
+
+    // creating particles in a grid of particles from (0,0,0) to (width,-height,0)
+    for (int x = 0; x < m_width; ++x) {
+        for (int y = 0; y < m_height; ++y) {
+            glm::vec3 position = glm::vec3(1.f * (x / (float)m_width), -1.f * (y / (float)m_height), 0.f);
+            m_particles[y * m_width + x] = Particle(position);
+        }
+    }
+
+    // Connecting immediate neighbor
+    for (int x = 0; x < m_width; ++x) {
+        for (int y = 0; y < m_height; ++y) {
+            if (x < m_width - 1) m_constraint.emplace_back(Constraint(get_particle(x, y), get_particle(x + 1, y)));
+            if (y < m_height - 1) m_constraint.emplace_back(Constraint(get_particle(x, y), get_particle(x, y + 1)));
+            if (x < m_width - 1 && y < m_height - 1) {
+                m_constraint.emplace_back(Constraint(get_particle(x, y), get_particle(x + 1, y + 1)));
+                m_constraint.emplace_back(Constraint(get_particle(x + 1, y), get_particle(x, y + 1)));
+            }
+        }
+    }
+
+    // Connecting secondary neighbors
+    for (int x = 0; x < m_width; ++x) {
+        for (int y = 0; y < m_height; ++y) {
+            if (x < m_width - 2) m_constraint.emplace_back(Constraint(get_particle(x, y), get_particle(x + 2, y)));
+            if (y < m_height - 2) m_constraint.emplace_back(Constraint(get_particle(x, y), get_particle(x, y + 2)));
+            if (x < m_width - 2 && y < m_height - 2) {
+                m_constraint.emplace_back(Constraint(get_particle(x, y), get_particle(x + 2, y + 2)));
+                m_constraint.emplace_back(Constraint(get_particle(x + 2, y), get_particle(x, y + 2)));
+            }
+        }
+    }
+
+    // disable top 3 particle to hold cloth
+    for (int i = 0; i< 3; i++) {
+        get_particle(0 + i, 0)->offset_pos(glm::vec3(0.5,0.0,0.0));
+        get_particle(0 + i, 0)->set_movable(false);
+
+        get_particle(0+i ,0)->offset_pos(glm::vec3(-0.5,0.0,0.0));
+        get_particle(0 + m_width - 1 - i ,0)->set_movable(false);
+    }
+    initVertex();
+}
+
+Cloth::~Cloth() {
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+}
+
+void Cloth::initVertex() {
+    std::vector<glm::vec3> data_buffer{};
+    for (int x = 0; x < m_width - 1; x++) {
+        for (int y = 0; y < m_height - 1; y++) {
+            data_buffer.emplace_back(get_particle(x + 1, y)->get_position());
+            data_buffer.emplace_back(get_particle(x, y)->get_position());
+            data_buffer.emplace_back(get_particle(x, y + 1)->get_position());
+
+            data_buffer.emplace_back(get_particle(x + 1, y + 1)->get_position());
+            data_buffer.emplace_back(get_particle(x + 1, y)->get_position());
+            data_buffer.emplace_back(get_particle(x, y + 1)->get_position());
+        }
+    }
+
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    int STRIDE = 3 * sizeof(float);
+    glBufferData(GL_ARRAY_BUFFER, data_buffer.size() * STRIDE, &data_buffer[0], GL_DYNAMIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, STRIDE, (void *)0);
+}
+
+void Cloth::add_wind_force(const glm::vec3& direction) {
+    for (int x = 0; x < m_width - 1; ++x) {
+        for (int y = 0; y < m_height - 1; ++y) {
+            add_wind_force_for_triangle(get_particle(x + 1, y), get_particle(x, y), get_particle(x, y + 1), direction);
+            add_wind_force_for_triangle(get_particle(x + 1, y + 1), get_particle(x + 1, y), get_particle(x, y + 1), direction);
+        }
+    }
+}
+
+glm::vec3 Cloth::calc_triangle_normal(Particle *p1, Particle *p2, Particle *p3) const {
+    glm::vec3 position1 = p1->get_position();
+    glm::vec3 position2 = p2->get_position();
+    glm::vec3 position3 = p3->get_position();
+
+    glm::vec3 v1 = position2 - position1;
+    glm::vec3 v2 = position3 - position1;
+
+    return glm::cross(v1, v2);
+}
+
+void Cloth::add_wind_force_for_triangle(Particle* p1, Particle* p2, Particle* p3, const glm::vec3& direction) {
+    glm::vec3 normal = calc_triangle_normal(p1, p2, p3);
+    glm::vec3 d = glm::normalize(normal);
+    glm::vec3 force = normal * glm::dot(d, direction);
+    p1->add_force(force);
+    p2->add_force(force);
+    p3->add_force(force);
+}
+
+void Cloth::render() {
+    std::vector<glm::vec3> data_buffer{};
+    for (int x = 0; x < m_width - 1; x++) {
+        for (int y = 0; y < m_height - 1; y++) {
+            data_buffer.emplace_back(get_particle(x + 1, y)->get_position());
+            data_buffer.emplace_back(get_particle(x, y)->get_position());
+            data_buffer.emplace_back(get_particle(x, y + 1)->get_position());
+
+            data_buffer.emplace_back(get_particle(x + 1, y + 1)->get_position());
+            data_buffer.emplace_back(get_particle(x + 1, y)->get_position());
+            data_buffer.emplace_back(get_particle(x, y + 1)->get_position());
+        }
+    }
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, data_buffer.size() * 3 * sizeof(float), &data_buffer[0]);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glDrawArrays(GL_TRIANGLES, 0, data_buffer.size());
+}
+
+void Cloth::update(float dt) {
+    if (!m_enabled) return;
+
+    for (int i = 0; i < m_cloth_solver_freq; i++) {
+        for (auto& constraint : m_constraint) {
+            constraint.satisfy();
+        }
+    }
+
+    glm::vec3 gravity_dir = glm::vec3(0.f, -0.2f, 0.f);
+    for (auto& particle : m_particles) {
+        if (m_use_gravity) {
+            particle.add_force(gravity_dir * dt);
+        }
+        particle.update(dt);
+    }
+}
+
+Particle* Cloth::get_particle(int x, int y) {
+    return &m_particles[y * m_width + x];
+}
+
 std::vector<float> vertices = {
     -0.5f,  0.5f, 0.0f, //vertex 1 : Top-left
     0.5f, 0.5f, 0.0f, //vertex 2 : Top-right
@@ -14,6 +257,8 @@ std::vector<float> vertices = {
 };
 
 unsigned vao1, vbo1, program1;
+Cloth* cloth;
+glm::vec3 dir = glm::vec3(0.5,0,0.2);
 
 bool loadFile(const std::string& filepath, std::string& out_source) {
     FILE* fp = nullptr;
@@ -103,6 +348,8 @@ bool init() {
     if (!program1)
         return false;
 
+    cloth = new Cloth(55,45);
+
     return true;
 }
 
@@ -110,18 +357,26 @@ void destroy() {
     glDeleteVertexArrays(1, &vao1);
     glDeleteBuffers(1, &vbo1);
     glDeleteProgram(program1);
+    if (cloth) {
+        delete cloth;
+        cloth = nullptr;
+    }
 }
 
 void update(float dt) {
-
+    cloth->add_wind_force(dir);
+    cloth->update(dt);
 }
 
 void render() {
     glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(-0.5f, 0.5f, 0.f));
+
     glUseProgram(program1);
-    glBindVertexArray(vao1);
+//    glBindVertexArray(vao1);
     glUniformMatrix4fv(glGetUniformLocation(program1, "model"), 1, GL_FALSE, glm::value_ptr(model));
-    glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+//    glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+    cloth->render();
 }
 
 int main() {
@@ -158,20 +413,25 @@ int main() {
         return -1;
     }
 
+    glEnable(GL_DEPTH_TEST);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
     while (!glfwWindowShouldClose(window)) {
         if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
             glfwSetWindowShouldClose(window, true);
 
-        update(1.f);
+        float fixed_timestamp = 0.25f;
+        update(fixed_timestamp);
 
         glClearColor(0, 0, 0, 0);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         render();
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+    destroy();
     glfwTerminate();
 
     return 0;
